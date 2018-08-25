d5afb95ed58ea19ab0091b2bb6e9664c170336d4
SriAnusha Kuchipudi <anusha@Anushas-MBP-2.lan>
2018-08-25 01:16:19 -0700

    Deploy Friend Finder App

---
+++ app/data/friend.js
@@ -0,0 +1,21 @@
+
+var friendList = [
+    {
+      name: 'Rose',
+      profilePic: 'https://images.pexels.com/photos/35646/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      scores: [
+        "5",
+        "4",
+        "5",
+        "3",
+        "2",
+        "3",
+        "3",
+        "4",
+        "5",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
